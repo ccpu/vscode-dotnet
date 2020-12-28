6c39806dce7fd8709fb978637debb9c17694288a
ccpu <m.doaie@hotmail.com>
2020-12-28 10:51:15 +0330

    bump packages disable Telemetry

--- src/appInsightsClient.ts
+++ src/appInsightsClient.ts
@@ -4,12 +4,17 @@ import * as vscode from "vscode";
 import { Utility } from "./utility";
 
 export class AppInsightsClient {
-    public static sendEvent(eventName: string, properties?: { [key: string]: string; }): void {
-        if (this._enableTelemetry) {
-            this._client.trackEvent({ name: eventName, properties });
-        }
+  public static sendEvent(
+    eventName: string,
+    properties?: { [key: string]: string }
+  ): void {
+    if (this._enableTelemetry) {
+      this._client.trackEvent({ name: eventName, properties });
     }
+  }
 
-    private static _client = new appInsights.TelemetryClient("2bfdfc27-d05c-4d8e-b628-4b0dc7b9a67e");
-    private static _enableTelemetry = Utility.getConfiguration().get<boolean>("enableTelemetry");
+  private static _client = new appInsights.TelemetryClient(
+    "2bfdfc27-d05c-4d8e-b628-4b0dc7b9a67e"
+  );
+  private static _enableTelemetry = false;
 }
